fa2372c89ffb040fe8acecf09d0e25805ba79458
troncosojose22 <troncosojose22@gmail.com>
2025-09-30 12:54:27 -0400

    TIP control flow practice

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ Below 40 → "Wear a heavy coat, scarf, and boots."
 
 const outfitPicker = (temp) => {
   // ✍️ Write your code here
+  if (temp >= 80) {
+    return 'Wear shorts and a t-shirt.';
+  } else if (temp >= 60 && temp <= 79) {
+    return 'Wear jeans and a light jacket.';
+  } else if (temp >= 40 && temp <= 59) {
+    return 'Wear a sweater and pants.';
+  } else {
+    return 'Wear a heavy coat, scarf and boots.';
+  }
 };
 
 //Test your function below
+
+console.log(outfitPicker(1));
+console.log(outfitPicker(81));
